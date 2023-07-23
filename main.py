48148cde0101e8a2826cd8590f6d64bdbf4078d9
tmk <morimoto.ke@gmail.com>
2023-07-23 22:29:28 +0900

    ✨ settingファイルの追加

---
+++ main.py
@@ -0,0 +1,17 @@
+import os
+import sys
+from datetime import datetime
+
+import numpy as np
+import pandas as pd
+
+DIR = os.path.join(".")
+
+
+a = 1
+b = 3
+
+
+print(a)
+if __name__ == "__main__":
+    pass
